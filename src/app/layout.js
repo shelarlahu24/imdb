d73076c05152ec5d4e904d0f3a8a1394d776aff7
shelarlahu24 <95674448+shelarlahu24@users.noreply.github.com>
2023-06-03 13:43:14 +0530

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Providers from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,15 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Providers>
+          {/* Header */}
+          <Header />
 
-        {/* Header */}
-        <Header />
+          {/* Navbar */}
 
-        {/* Navbar */}
+          {/* Searchbar */}
 
-        {/* Searchbar */}
-
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   )
